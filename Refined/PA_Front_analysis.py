01d9ea33762c62d0657fcc8127a9fc1e4d6ce7ea
Wobbe <55002389+Mystellic@users.noreply.github.com>
2020-03-18 10:15:32 +0100

    Imported Refined data; Created Contourf function

--- Refined/PA_Front_analysis.py
+++ Refined/PA_Front_analysis.py
@@ -4,4 +4,74 @@ Created on Tue Mar 17 14:36:37 2020
 
 @author: myste
 """
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import glob
+import os
 
+def ReadInfo(filename):
+    #imports notepad
+    try:
+        infographic = np.array(pd.read_csv(filename, sep = "\t"))
+    except:
+        print("File was not found")
+    return infographic
+
+def ReadData(folder):
+    files = np.array([])
+    for name in glob.glob(folder + '\\*'):
+        files = np.append(files,name)
+    return files
+
+def FileToArray(fileThing):
+    newArray = []
+    for element in fileThing:
+        newPart = RunFile(element)
+        newArray.append(newPart)
+        useArray = np.array(newArray)
+    return useArray
+
+def RunFile(fileName):
+    file_list = np.array([])
+    data = pd.read_csv(fileName,header=None)
+    data = np.nan_to_num(np.array(data))
+    return data
+
+    
+def CreateImage(pixelFrame):
+    levels = np.linspace(np.amin(pixelFrame),np.amax(pixelFrame),30) 
+    plt.contourf(pixelFrame,levels,cmap='nipy_spectral')
+    plt.show()
+
+def CleanCorrection(pictureArray,cleanInfo,allInfo):
+    for i in range(pictureArray):
+        #check which clean case needs to be subtracted
+    
+    return CorrectedArray
+
+def GenerateClean(info):
+    clean = [0,0,0]
+    for element in info:
+        if element[2] == element[3] == 0:
+            series_day = [int(element[0][1:]),element[6]]
+            clean[element[6]-1] = series_day
+
+def main():
+    info1 = ReadInfo('Info_PA1.txt')
+    info2 = ReadInfo('Info_PA2.txt')
+    info3 = ReadInfo('Info_PA3.txt')
+    
+    
+    cleanCases = GenerateClean(info1)
+    
+    
+            
+    
+
+main()
+    
+        
+
+#PA1 = FileToArray(ReadData('PA1'))
+#CreateImage(PA1[0])
